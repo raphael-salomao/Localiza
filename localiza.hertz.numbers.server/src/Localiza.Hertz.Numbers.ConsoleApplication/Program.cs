d0baed15289712df31cf10654cef4810ae8eb1d7
raphael-salomao <raphael.bonfim.salomao@gmail.com>
2020-12-14 09:26:51 -0300

    Criação do um console

---
+++ localiza.hertz.numbers.server/src/Localiza.Hertz.Numbers.ConsoleApplication/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Localiza.Hertz.Numbers.ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, essa aplicação recebe o número informado e retorna os seus divisores e dentre os divisores os que são primos!");
+
+            Menu();
+        }
+
+        static void Menu()
+        {
+            decimal number;
+
+            Console.WriteLine("\nDigite 0 para iniciar ou 1 para sair: ");
+
+            try
+            {
+                number = Convert.ToDecimal(Console.ReadLine());
+
+                switch (number)
+                {
+                    case 0:
+                        Start();
+                        break;
+                    case 1:
+                        System.Environment.Exit(-1);
+                        break;
+                    default:
+                        Menu();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Entrada inválida!");
+            }
+        }
+
+        static void Start()
+        {
+            decimal number;
+
+            Console.WriteLine("\nInforme o número maior que zero: ");
+
+            try
+            {
+                number = Convert.ToDecimal(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Entrada inválida!");
+            }
+        }
+    }
+}
